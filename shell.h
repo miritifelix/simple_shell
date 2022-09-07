187288010f661c43f9a999383d0ef221aa2ed546
vincenttommi <vincenttommi@gmail.com>
2022-09-07 23:12:32 +0300

    error

--- shell.h
+++ shell.h
@@ -152,6 +152,7 @@ char *_strchr(char *, char);
 char **strtow(char *, char *);
 char **strtow2(char *, char);
 
+
 /* toem_realloc.c */
 char *_memset(char *, char, unsigned int);
 void ffree(char **);
